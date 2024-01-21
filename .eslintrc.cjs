601b3ad234faba818a0c0eea976ed5183e6d3b58
sanpicule <sannsi4444@gmail.com>
2024-01-21 22:11:11 +0900

    不要な設定の削除

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,36 +27,5 @@ module.exports = {
   plugins: ['@typescript-eslint', 'react'],
   rules: {
     'react/react-in-jsx-scope': 'off',
-    // 暗黙の型強制を禁止し安全性を高めるルール
-    '@typescript-eslint/strict-boolean-expressions': [
-      'warn',
-      { allowString: false, allowNumber: false, allowNullableObject: false },
-    ],
-    'no-implicit-coercion': 'error',
-    '@typescript-eslint/restrict-plus-operands': [
-      'error',
-      {
-        checkCompoundAssignments: true,
-        allowBoolean: false,
-        allowNullish: false,
-        allowNumberAndString: false,
-        allowRegExp: false,
-        allowAny: false,
-      },
-    ],
-    'prefer-template': 'error',
-    // template literal に使用できる型の制限
-    '@typescript-eslint/restrict-template-expressions': [
-      'error',
-      {
-        allowNumber: true,
-        allowBoolean: true,
-        allowNullable: false,
-        allowAny: false,
-        allowNever: false,
-        allowNullish: false,
-        allowRegExp: false,
-      },
-    ],
   },
 }
