ca87c6b4883d733179a5009400d4172a27c66097
sanpicule <sannsi4444@gmail.com>
2024-01-21 18:59:40 +0900

    テストサンプル実装

---
+++ src/components/TextInput.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+const TextInput = () => {
+  const [text, setText] = useState('')
+  return (
+    <div>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        aria-label='Input Element'
+      />
+      <p>Entered Text: {text}</p>
+    </div>
+  )
+}
+
+export default TextInput
